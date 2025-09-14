d4716063aeb3f62c93280265e2fde151833f6c1c
JLarky <jlarky@gmail.com>
2025-09-14 09:18:48 -0600

    use htmx to preserve input (breaks on slow network)
    - https://github.com/bigskysoftware/htmx/issues/75#issuecomment-642668200

--- src/components/web-components/TableHeader.ts
+++ src/components/web-components/TableHeader.ts
@@ -1,5 +1,4 @@
-import { liftSolid, useAttributes } from "@lift-html/solid";
-import { createEffect, onCleanup } from "solid-js";
+import { liftSolid } from "@lift-html/solid";
 import { targetRefs } from "@lift-html/incentive";
 import { debounce } from "@solid-primitives/scheduled";
 
@@ -7,15 +6,10 @@ import.meta.hot?.accept();
 
 const elementName = "table-header";
 
-const focusState = new Map<string, { pos: number | null; text: string }>();
-
 const TableHeader = liftSolid(elementName, {
-  observedAttributes: ["field"] as const,
   init(dispose) {
     const abortController = new AbortController();
     dispose(() => abortController.abort());
-
-    const props = useAttributes(this);
 
     const targets = targetRefs(this, {
       input: HTMLInputElement,
@@ -35,49 +29,11 @@ const TableHeader = liftSolid(elementName, {
     const trigger = debounce(onSearchInput, 300);
 
     inputEl.addEventListener("input", trigger, abortController);
-
-    createEffect(() => {
-      const field = props.field;
-      if (!field) throw new Error(elementName + " attribute field is required");
-      const abortController = new AbortController();
-      onCleanup(() => abortController.abort());
-
-      document.body.addEventListener(
-        "htmx:beforeSwap",
-        () => {
-          const active = document.activeElement as HTMLInputElement | null;
-          const pos = active === inputEl ? inputEl.selectionStart : null;
-          focusState.set(field, { pos, text: inputEl.value });
-        },
-        abortController,
-      );
-
-      document.body.addEventListener(
-        "htmx:afterSwap",
-        () => {
-          const { pos, text } = focusState.get(field) ?? {
-            pos: null,
-            text: "",
-          };
-          if (pos !== null) {
-            inputEl.focus();
-            if (text !== inputEl.value) {
-              inputEl.value = text;
-              trigger();
-            }
-            inputEl.setSelectionRange(pos, pos);
-          }
-        },
-        abortController,
-      );
-    });
   },
 });
 
 declare module "@lift-html/core" {
   interface KnownElements {
-    [elementName]: typeof TableHeader & {
-      props: { field: string };
-    };
+    [elementName]: typeof TableHeader & { props: {} };
   }
 }
